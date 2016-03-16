1796595d193106531bf417e2db2a3393eddacbd8
Dave Townsend <dtownsend@oxymoronical.com>
2016-03-15 20:47:49 -0700

    Attempt to force thumbnails.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,30 +1,39 @@
 def Start():
-    pass
+    Plugin.AddViewGroup("Thumbs", viewMode="PosterList", mediaType="items", type=ViewType.Grid)
 
 @handler("/photos/smartmedia", "Smart Media", thumb="smartmedia.png")
 def Main():
-    oc = ObjectContainer(title1="Smart Media")
+    oc = ObjectContainer(title1="Smart Media", content=ContainerContent.Mixed)
 
     url = "%slist/roots" % (Prefs["url"])
     json = JSON.ObjectFromURL(url)
     for root in json:
         modelthumb=R("%s.png" % root["model"])
-        oc.add(DirectoryObject(key = Callback(Root, model=root["model"], title1=root["name"]), title=root["name"], thumb=modelthumb))
+        oc.add(PhotoAlbumObject(
+            key = Callback(Root, model=root["model"], title1=root["name"]),
+            rating_key=root["model"], title=root["name"], thumb=modelthumb
+        ))
 
     return oc
 
 def ListFolder(title1, title2, model, url):
-    oc = ObjectContainer(title1=title1, title2=title2)
+    oc = ObjectContainer(title1=title1, title2=title2, content=ContainerContent.Mixed)
 
     json = JSON.ObjectFromURL(url)
     for obj in json:
         if obj["model"] == "website.photo":
-            imgurl = "%sphoto/%s/download" % (Prefs["url"], obj["pk"])
-            thumburl = "%sphoto/%s/thumbnail/512" % (Prefs["url"], obj["pk"])
-            oc.add(PhotoObject(key=imgurl, rating_key=imgurl, title=obj["fields"]["filename"], thumb=thumburl))
+            imgurl = obj["fields"]["url"]
+            thumburl = "%sphoto/%s/thumbnail/300" % (Prefs["url"], obj["pk"])
+            oc.add(PhotoObject(
+                key=imgurl, rating_key=imgurl, title=obj["fields"]["filename"], thumb=thumburl)
+            )
         else:
             modelthumb=R("%s.png" % model)
-            oc.add(DirectoryObject(key = Callback(Folder, model=model, id=obj["pk"], title1=title1, title2=obj["fields"]["name"]), title=obj["fields"]["name"], thumb=modelthumb))
+            key="%s-%s" % (model, obj["pk"])
+            oc.add(PhotoAlbumObject(
+                key = Callback(Folder, model=model, id=obj["pk"], title1=title1, title2=obj["fields"]["name"]),
+                rating_key=key, title=obj["fields"]["name"], thumb=modelthumb)
+            )
 
     return oc
 
